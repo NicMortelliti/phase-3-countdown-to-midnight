b69ac78e67dbe4814b3394ca07881700a9ad1485
NicMortelliti <npmortelliti@gmail.com>
2022-06-01 18:34:53 -0700

    Assignment complete

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  x = 1
+  while int >= x
+    puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  x = 1
+  while int >= x
+    puts "#{int} SECOND(S)!"
+    sleep(1)
+    int -= 1
+  end
 end
